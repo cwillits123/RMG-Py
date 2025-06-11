cf26d4a6ca83bcec7f3064af490e32d607f02f89
Colin Willits <willitscb@MacBook-Air.local>
2025-06-11 11:06:43 -0400

    ran some rmg and added ch2f2

---
+++ examples/rmg/catalysis/ch4_o2/restart_from_seed.py
@@ -0,0 +1,89 @@
+restartFromSeed(path='seed')
+
+# Data sources
+database(
+    thermoLibraries=['surfaceThermoPt111', 'primaryThermoLibrary', 'thermo_DFT_CCSDTF12_BAC','DFT_QCI_thermo'], # 'surfaceThermoPt' is the default. Thermo data is derived using bindingEnergies for other metals 
+    reactionLibraries = [('Surface/CPOX_Pt/Deutschmann2006_adjusted', False)], # when Ni is used change the library to Surface/Deutschmann_Ni 
+    seedMechanisms = [],
+    kineticsDepositories = ['training'],
+    kineticsFamilies = ['surface','default'],
+    kineticsEstimator = 'rate rules',
+
+)
+
+catalystProperties(
+    metal = 'Pt111'
+)
+
+species(
+    label='CH4',
+    reactive=True,
+    structure=SMILES("[CH4]"),
+)
+
+species(
+   label='O2',
+   reactive=True,
+   structure=adjacencyList(
+       """
+1 O u1 p2 c0 {2,S}
+2 O u1 p2 c0 {1,S}
+"""),
+)
+
+species(
+    label='N2',
+    reactive=False,
+    structure=SMILES("N#N"),
+)
+
+species(
+    label='vacantX',
+    reactive=True,
+    structure=adjacencyList("1 X u0"),
+)
+
+# If you would like to forbid the bidentate form of absorbed CO2 from your model,
+# use the following `CO2_bidentate` forbidden structure
+# forbidden(
+#     label='CO2_bidentate',
+#     structure=SMILES("O=C(*)O*"),
+# )
+
+#----------
+# Reaction systems
+surfaceReactor(
+    temperature=(800,'K'),
+    initialPressure=(1.0, 'bar'),
+    initialGasMoleFractions={
+        "CH4": 0.1,
+        "O2": 0.2,
+        "N2": 0.7,
+    },
+    initialSurfaceCoverages={
+        "vacantX": 1.0,
+    },
+    surfaceVolumeRatio=(1.e5, 'm^-1'),
+    terminationConversion = { "CH4":0.99,},
+    terminationTime=(0.1, 's'),
+)
+
+simulator(
+    atol=1e-18,
+    rtol=1e-12,
+)
+
+model(
+    toleranceKeepInEdge=0.0,
+    toleranceMoveToCore=1e-5,
+    toleranceInterruptSimulation=0.1,
+    maximumEdgeSpecies=100000,
+)
+
+options(
+    units='si',
+    generateOutputHTML=True,
+    generatePlots=False, # Enable to make plots of core and edge size etc. But takes a lot of the total runtime!
+    saveEdgeSpecies=True,
+    saveSimulationProfiles=True,
+)
